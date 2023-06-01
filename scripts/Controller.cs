9db093273d0f4150ed9c213312dd73945f4f1a06
GustavoMede <gustavomedeiros0708@outlook.com>
2023-05-31 23:06:02 -0300

    adding mangos joseph and saw enemy animations

--- scripts/Controller.cs
+++ scripts/Controller.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Controller : MonoBehaviour
 {
@@ -10,6 +11,11 @@ public class Controller : MonoBehaviour
     public LayerMask layerChao;
     private bool estaNoChao;
     public float distanciaMinimaChao;
+    private float pontos;
+    private float multiplicadorPontos = 4.5f;
+    public Text pontosText;
+    public Animator animatorComponent;
+    public float limiarDeQueda = -3f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,26 +26,41 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow)) 
-        {
-            pular();
-        }
+        pontos += Time.deltaTime * multiplicadorPontos;
+        pontosText.text = Mathf.FloorToInt(pontos).ToString();
+        if (Input.GetKeyDown(KeyCode.UpArrow)) pular();
     }
 
    private void pular() 
     {
         if (estaNoChao) {
             rigidBody.AddForce(Vector2.up * forcaPulo);
+            startAnimation("Pulando");
         }
     }
 
     private void FixedUpdate() {
-        estaNoChao = Physics2D.Raycast(transform.position, Vector2.down, distanciaMinimaChao, layerChao);
+        validaPlayerNoChao();
+        if (rigidBody.velocity.y < limiarDeQueda) startAnimation("Caindo");
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
-        if (other.gameObject.CompareTag("Inimigo")) {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
+        if (other.gameObject.CompareTag("Inimigo")) SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void validaPlayerNoChao() {
+        estaNoChao = Physics2D.Raycast(transform.position, Vector2.down, distanciaMinimaChao, layerChao);
+        startAnimation("Andando");
+    }
+
+    private void startAnimation(string animation) {
+        invalidateAnimations();
+        animatorComponent.SetBool(animation, true);
+    }
+
+    private void invalidateAnimations() {
+        animatorComponent.SetBool("Pulando", false);
+        animatorComponent.SetBool("Caindo", false);
+        animatorComponent.SetBool("Andando", false);
     }
 }
