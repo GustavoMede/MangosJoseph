b7eff7cc00f00d356ff17984d61b47df97074649
GustavoMede <gustavomedeiros0708@outlook.com>
2023-06-19 18:47:48 -0300

    add ze da manga sound

--- scripts/DialogController.cs
+++ scripts/DialogController.cs
@@ -7,21 +7,39 @@ public class DialogController : MonoBehaviour
 
     private bool isGamePaused = true;
     public Text cliqueParaIniciarText;
+    public Image direcionaisTutorialImage;
+    public Text pularText;
+    public Text agacharText;
 
     void Start()
     {
-        Time.timeScale = 0f;
-        dialogBubble.SetActive(true);
+        pausaGame();
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            cliqueParaIniciarText.enabled = false;
-            dialogBubble.SetActive(false);
-            Time.timeScale = 1f;
-            isGamePaused = false;
+            desativaDialogos();
+            iniciaJogo();
         }
+    }
+
+    private void pausaGame() {
+        Time.timeScale = 0f;
+        dialogBubble.SetActive(true);
+    }
+
+    private void desativaDialogos() {
+        direcionaisTutorialImage.enabled = false;
+        pularText.enabled = false;
+        agacharText.enabled = false;
+        cliqueParaIniciarText.enabled = false;
+        dialogBubble.SetActive(false);
+    }
+
+    private void iniciaJogo() {
+        Time.timeScale = 1f;
+        isGamePaused = false;
     }
 }
